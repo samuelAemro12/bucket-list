704c5ac2ed77bca20a195dfacf0a31c1cc77550c
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-25 03:53:36 -0700

    add prompt and personalixed welcome

--- src/bucketlist/BucketList.java
+++ src/bucketlist/BucketList.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class BucketList {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Bucket List");
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "! Let's build your bucket list.");
     }
 }
