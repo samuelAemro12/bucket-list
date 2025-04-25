5aa7e211a5afb84a0282833c006d4da0d239dcb5
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-25 04:00:33 -0700

    refactor: move name and age logic into Travel class

--- src/bucketlist/BucketList.java
+++ src/bucketlist/BucketList.java
@@ -2,16 +2,28 @@ import java.util.Scanner;
 
 public class BucketList {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Bucket List");
 
+        Travel travel = new Travel();
+        travel.visit();
+    }
+}
+
+class Travel {
+    String name;
+    int age;
+
+    Travel() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
+        this.name = sc.nextLine();
         System.out.print("Enter your age: ");
-        int age = sc.nextInt();
+        this.age = sc.nextInt();
+    }
 
+    public void visit() {
         System.out.println("\nHello " + name + "! Let's build your bucket list.");
+        System.out.println("======= THIS IS " + name + "'s BUCKET LIST =======");
 
         if (age < 30) {
             System.out.println("- Visit Old Trafford");
