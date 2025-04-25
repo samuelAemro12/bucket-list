6efb5b6110fccabd0e633f05f3a13fe965600439
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-25 03:59:37 -0700

    add age input and display bucket list based on age range

--- src/bucketlist/BucketList.java
+++ src/bucketlist/BucketList.java
@@ -4,8 +4,22 @@ public class BucketList {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Bucket List");
+
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
-        System.out.println("Hello " + name + "! Let's build your bucket list.");
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("\nHello " + name + "! Let's build your bucket list.");
+
+        if (age < 30) {
+            System.out.println("- Visit Old Trafford");
+            System.out.println("- Go Trekking on Mount Ras Dejen");
+        } else if (age <= 45) {
+            System.out.println("- Go on a Pilgrimage to Lalibela");
+        } else {
+            System.out.println("- Reflect and travel the world with wisdom.");
+        }
     }
 }
