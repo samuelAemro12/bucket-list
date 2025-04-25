560b0761a79a4c976c4c6543c11e8e4cfdd8b05b
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-25 09:53:01 -0700

    Encapsulate age validation into a separate method for cleaner logic and corrected main package declaration

--- src/bucketlist/BucketList.java
+++ src/bucketlist/BucketList.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
+package bucketlist;
+
+import java.util.*;
 
 public class BucketList {
     public static void main(String[] args) {
         System.out.println("Welcome to the Bucket List");
+        System.out.println("Enter your name");
 
         Travel travel = new Travel();
         travel.visit();
@@ -11,81 +14,72 @@ public class BucketList {
         display.forFather();
         display.forThySelf();
         display.rule();
+
         Hunt hunt = new Hunt();
         hunt.rule();
-
     }
 }
+
+class Travel {
+    private int age;
+    private String name;
 
     Travel() {
         Scanner sc = new Scanner(System.in);
         this.name = sc.nextLine();
+        System.out.println("Enter your age:");
+        this.age = getValidAge(sc);
+    }
 
-        System.out.print("Enter your age: ");
-        while (true) {
-            try {
-                this.age = Integer.parseInt(sc.nextLine());
-                if (age > 0) break;
-                else System.out.print("Please enter a valid age: ");
-            } catch (NumberFormatException e) {
-                System.out.print("Invalid input. Please enter a number: ");
-            }
+    private int getValidAge(Scanner sc) {
+        int inputAge = sc.nextInt();
+        while (inputAge <= 0) {
+            System.out.println("Please enter a valid age:");
+            inputAge = sc.nextInt();
         }
+        return inputAge;
     }
 
     public void visit() {
-        System.out.println("\nHello " + name + "! Let's build your bucket list.");
         System.out.println("======= THIS IS " + name + "'s BUCKET LIST =======");
 
-        if (age < 30) {
-            System.out.println("- Visit Old Trafford");
-            System.out.println("- Go Trekking on Mount Ras Dejen");
-        } else if (age <= 45) {
-            System.out.println("- Go on a Pilgrimage to Lalibela");
+        if (age > 0 && age < 30) {
+            System.out.println("Visit Old Trafford");
+            System.out.println("Go Treking on Mount Ras Dejen");
+        } else if (age >= 30 && age <= 45) {
+            System.out.println("Go on Pilgrimage to Lalibella");
         } else {
-            System.out.println("- Reflect and travel the world with wisdom.");
+            System.out.println("Live your life to the fullest, age is just a number!");
         }
     }
 }
 
 abstract class Become {
-    abstract public void forFather();
-    abstract public void forThySelf();
+    String character = "HONOURABLE";
+    public abstract void forFather();
+    public abstract void forThySelf();
 }
 
 class Display extends Become {
     public void forFather() {
-        System.out.println("- Make Father Truly Proud");
+        System.out.println("Make Father Truly Proud");
     }
 
     public void forThySelf() {
-        System.out.println("- Find My Passion");
-        System.out.println("- Excel at my Field");
-        System.out.println("- Finish My TO-READ LIST");
-        System.out.println("- Coach a football team");
-    }
-}
-
-class Display extends Become {
-    public void forFather() {
-        System.out.println("- Make Father Truly Proud");
-    }
-
-    public void forThySelf() {
-        System.out.println("- Find My Passion");
-        System.out.println("- Excel at my Field");
-        System.out.println("- Finish My TO-READ LIST");
-        System.out.println("- Coach a football team");
+        System.out.println("Find My Passion");
+        System.out.println("Excel at my Field");
+        System.out.println("Finish My TO-READ LIST");
+        System.out.println("Coach a football team");
     }
 
     public void rule() {
-        System.out.println("- Rule my Life");
-        System.out.println("- Rule my Children as the Laws of the Lord dictates");
+        System.out.println("Rule my Life");
+        System.out.println("Rule my Children as the Laws of the Lord dictate");
     }
 }
+
 class Hunt extends Display {
-    @Override
     public void rule() {
-        System.out.println("- Rule The Career World");
+        System.out.println("Rule The Career World");
     }
 }
